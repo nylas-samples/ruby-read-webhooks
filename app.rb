0cf59a5b070b729f4e4543f35b12d66ceb6f3667
Blag <atejada@gmail.com>
2023-09-07 11:55:33 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -34,13 +34,30 @@ query_params = {
 # We read the webhook information and store it on the data class
     request.body.rewind
     model = JSON.parse(request.body.read)
-    event, _request_id = nylas.events.find(identifier: ENV["CALENDAR_ID"], object_id: model["data"]["object"]["id"], query_params: query_params) 
-	participants = ""
-	event[:participants].each do |elem|
-	    participants += "#{elem[:email]}; "
-	end
-    hook = webhook.new(event[:id], event[:date], event[:title], event[:description], participants, event[:status])
-    webhooks.append(hook)
+    if model["data"]["object"]["calendar_id"] == ENV["CALENDAR_ID"]
+        event, _request_id = nylas.events.find(identifier: ENV["CALENDAR_ID"], object_id: model["data"]["object"]["id"], query_params: query_params) 
+        participants = ""
+        event_date = ""
+        event[:participants].each do |elem|
+            participants += "#{elem[:email]}; "
+        end
+    
+        case event[:when][:object]
+            when 'timespan'
+	        start_time = Time.at(event[:when][:start_time]).strftime('%m-%d-%Y %H:%M:%S')
+	        end_time = Time.at(event[:when][:end_time]).strftime('%m-%d-%Y %H:%M:%S')
+	        event_date = "#{start_time} to #{end_time}"
+	    when 'datespan'
+                start_time = event[:when][:start_date].strftime('%m-%d-%Y')
+	        end_time = event[:when][:end_date].strftime('%m-%d-%Y')
+	        event_date = "#{start_time} to: #{end_time}"	    
+	    when 'date'
+	        start_time = event[:when][:date].strftime('%m-%d-%Y')
+	        event_date = "#{start_time} "
+        end
+        hook = webhook.new(event[:id], event_date, event[:title], event[:description], participants, event[:status])
+        webhooks.append(hook)
+    end
 	status 200
 	"Webhook received"	
 end
